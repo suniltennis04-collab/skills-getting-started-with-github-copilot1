3b36f90fecf31085c81ac8bc26c24c648ef9c2e3
suniltennis04-collab <suniltennis04@gmail.com>
2026-01-14 08:48:01 +0000

    Fix syntax error in activities dictionary definition

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 
 # In-memory activity database
 activities = {
-    ties = {
+
         "Chess Club": {
             "description": "Learn strategies and compete in chess tournaments",
             "schedule": "Fridays, 3:30 PM - 5:00 PM",
@@ -78,7 +78,7 @@ activities = {
         }
     }
     
-}
+
 
 
 @app.get("/")
